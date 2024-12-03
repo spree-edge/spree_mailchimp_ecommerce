5757820b5a360df9cd8c33adc761434384f3738c
sumit-bluebash <sumit.kumar@bluebash.co>
2024-12-03 11:44:17 +0530

    test mailchimp for claire by some product synced.

--- app/jobs/spree_mailchimp_ecommerce/upload_store_content_job.rb
+++ app/jobs/spree_mailchimp_ecommerce/upload_store_content_job.rb
@@ -6,22 +6,22 @@ module SpreeMailchimpEcommerce
 
 
         MailchimpSetting.last.update(state: 'syncing')
-        ::Spree::Product.find_each do |product|
+        ::Spree::Product.last(10).each do |product|
           ::SpreeMailchimpEcommerce::CreateProductJob.perform_now(product.mailchimp_product)
         end
 
-        ::Spree::User.where.not(email: nil).find_each do |user|
-          ::SpreeMailchimpEcommerce::CreateUserJob.perform_now(user.mailchimp_user)
-        end
+        # ::Spree::User.where.not(email: nil).find_each do |user|
+        #   ::SpreeMailchimpEcommerce::CreateUserJob.perform_now(user.mailchimp_user)
+        # end
 
         # ::Spree::Order.complete.find_each do |order|
         #   ::SpreeMailchimpEcommerce::CreateOrderJob.perform_now(order.mailchimp_order)
         # end
 
-        ::Spree::Promotion.find_each do |promotion|
-          ::SpreeMailchimpEcommerce::CreatePromoRuleJob.perform_now(promotion.mailchimp_promo_rule)
-          ::SpreeMailchimpEcommerce::CreatePromoCodeJob.perform_now(promotion.mailchimp_promo_rule, promotion.mailchimp_promo_code)
-        end
+        # ::Spree::Promotion.find_each do |promotion|
+        #   ::SpreeMailchimpEcommerce::CreatePromoRuleJob.perform_now(promotion.mailchimp_promo_rule)
+        #   ::SpreeMailchimpEcommerce::CreatePromoCodeJob.perform_now(promotion.mailchimp_promo_rule, promotion.mailchimp_promo_code)
+        # end
       rescue Gibbon::MailChimpError => e
         Rails.logger.error("[MAILCHIMP] Error while syncing process: #{e}")
       end
